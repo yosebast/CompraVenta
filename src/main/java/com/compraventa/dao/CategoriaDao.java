7536637f1b906b084d728c8bcac7e57bed79aadd
vladimir <ik_sbtian@yahoo.es>
2017-09-29 14:06:58 +0200

    ultima modificacion

--- src/main/java/com/compraventa/dao/CategoriaDao.java
+++ src/main/java/com/compraventa/dao/CategoriaDao.java
@@ -1,8 +1,13 @@
 package com.compraventa.dao;
 
+import java.util.List;
+
 import com.compraventa.entidades.Categoria;
+import com.compraventa.entidades.Seccion;
 	
-public interface CategoriaDao extends GenericDao<Categoria, Integer> {	
+public interface CategoriaDao extends GenericDao<Categoria, Integer> {
+
+	List<Categoria> getCategoryBySeccion(Seccion seccion);	
 	/*public void insertarCategoria(Categoria categoria);
 	public List<Categoria> listaCategorias();*/
 }
--- src/main/java/com/compraventa/dao/CategoriaDaoImpl.java
+++ src/main/java/com/compraventa/dao/CategoriaDaoImpl.java
@@ -1,12 +1,20 @@
 package com.compraventa.dao;
 
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 import com.compraventa.entidades.Categoria;
+import com.compraventa.entidades.Seccion;
 
 	
 @Repository
-public class CategoriaDaoImpl extends GenericDaoImpl<Categoria, Integer> implements CategoriaDao {	
+public class CategoriaDaoImpl extends GenericDaoImpl<Categoria, Integer> implements CategoriaDao {
+
+	public List<Categoria> getCategoryBySeccion(Seccion seccion) {
+		// TODO Auto-generated method stub
+		return null;
+	}	
 	
 
 }
